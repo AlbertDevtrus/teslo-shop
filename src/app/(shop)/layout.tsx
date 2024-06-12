69ced76c49a32c4d388d8c57d40fc45bbcab187f
Alberto Guzman <112503817+TimmyElTaco@users.noreply.github.com>
2024-06-12 07:24:24 -0500

    Create top menu

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,8 +1,10 @@
+import { TopMenu } from "@/components";
 
 
 export default function ShopLayout({children}: {children: React.ReactNode;}) {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 bg-red-500">
+    <main className="min-h-screen">
+      <TopMenu />
       {children}
     </main>
   );
